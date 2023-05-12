cc4e01da7b81a1f86bbcd2bcc986c8918af0fc4c
mindfu1ness <eridel61@yandex.ru>
2023-05-12 11:41:45 +0500

    Revert "Revert "Revert "///"""
    This reverts commit ff82e4a3f946672b464bdc25168dce21fce5a496.

--- Game/Form1.Designer.cs
+++ Game/Form1.Designer.cs
@@ -28,15 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.WindowState = FormWindowState.Maximized;
-            //this.FormBorderStyle = FormBorderStyle.None;
-            this.TopMost = true;
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
-            this.StartPosition = FormStartPosition.CenterParent;
-            //this.BackgroundImage = Resources.backgroundShapes;
         }
 
         #endregion
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -1,6 +1,3 @@
-using System.Drawing.Drawing2D;
-using Game.Model;
-
 namespace Game
 {
     public partial class Form1 : Form
@@ -8,37 +5,6 @@ namespace Game
         public Form1()
         {
             InitializeComponent();
-
-            SetStyle(ControlStyles.OptimizedDoubleBuffer
-                | ControlStyles.AllPaintingInWmPaint
-                | ControlStyles.UserPaint, true);
-
-            DoubleBuffered = true;
-            Model.Game.Initialize();
-
-            var timer = new System.Windows.Forms.Timer();
-            timer.Interval = 25;
-            timer.Tick += new EventHandler(Update);
-            timer.Start();
-        }
-
-        private void Update(object sender, EventArgs e)
-        {
-            Model.Game.State.Player.Update();
-            Invalidate();
-        }
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            var graphics = e.Graphics;
-            graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
-            graphics.PixelOffsetMode = PixelOffsetMode.Half;
-
-            foreach (var tile in Model.Game.State.Level.Tiles)
-                if (tile != null)
-                    graphics.DrawImage(tile.Sprite, tile.DrawingArea);
-
-            graphics.DrawImage(Model.Game.State.Player.Sprite, Model.Game.State.Player.DrawingArea);
         }
     }
 }
